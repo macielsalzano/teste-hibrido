c8eec3d86c80715e2a5b98c3456fb3f6303b9f0e
Maciel Salzano <macielsalzano@gmail.com>
2025-10-31 18:52:04 -0300

    Download do diretório app

---
+++ app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'mguadm'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'tkCabifA1fIgwxGAdfx44tMrW64pgk48'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '82f_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '82f_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base644gHnGhAxFdqPs9fS4lr4laH9U8ZPqgmqm/XNMkRSnDY='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'salzano_db',
+                'username' => 'salzano_user',
+                'password' => '09b25u63sp17rYm8',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'salzano.testsrv.mageuni.cloud'
+    ],
+    'install' => [
+        'date' => 'Fri, 31 Oct 2025 17:21:33 +0000'
+    ]
+];
